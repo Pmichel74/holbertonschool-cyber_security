73ea80560b23e141aef2f653de0c73c4dc740f3c
Your Name <you@example.com>
2025-08-11 15:15:39 +0200

    task1

---
+++ linux_security/0x00_linux_security_basics/1-active-connections.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script to display active network connections
+# Shows listening ports and established connections with process information
+
+#  ss command to show socket statistics
+# -t: TCP sockets
+# -n: numerical addresses
+# -p: process using the socket
+
+ss -tnp
