c6c07e508d11d4fc5cdacb9f3b9a255085cf85c6
Pmichel74 <patrick.yann.michel@gmail.com>
2025-08-13 12:03:48 +0200

    task5

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk -F': ' '/^(Registrant|Admin|Tech) (Name|Organization|Street|City|State\/Province|Postal Code|Country|Phone|Fax|Email|Phone Ext:|Fax Ext:)/{print $1"," $2}' > $1.csv
+whois $1 | awk -F":" '/^(Registrant|Admin|Tech) (Name|Organization|Street|City|State\/Province|Postal Code|Country|Phone|Fax|Email|Phone Ext:|Fax Ext:)/{print $1"," $2}' > $1.csv
