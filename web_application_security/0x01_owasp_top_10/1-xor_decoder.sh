14f4d58278a71b9812b27d4333feb17e040b90ab
Pmichel74 <patrick.yann.michel@gmail.com>
2025-09-16 12:22:48 +0200

    task1

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-data="${1#'{xor}'}"
-decoded=$(echo -n "$data" | base64 -d 2>/dev/null)
-for ((i=0; i<${#decoded}; i++)); do
-    char="${decoded:$i:1}"
-    printf "\\$(printf '%03o' $(($(printf "%d" "'$char") ^ 95)))"
-done
-echo
+echo ${1#*\}} | base64 -d 2>/dev/null | od -An -t u1 | awk '{for(i=1;i<=NF;i++)printf "%c", $i^53}'
