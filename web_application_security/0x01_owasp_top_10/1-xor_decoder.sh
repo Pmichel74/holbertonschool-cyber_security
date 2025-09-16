59dc0dea7bcfd9257bc3232de26239c9168a680c
Pmichel74 <patrick.yann.michel@gmail.com>
2025-09-16 12:12:22 +0200

    task1

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,5 +1,13 @@
-#!/bin/b# Decode the Base64 encoded string
-decoded_data=$(echo -n "$input_data" | openssl enc -base64 -d)
+#!/bin/bash
+
+# Get the first argument passed to the script
+input_data="$1"
+
+# Remove the {xor} prefix from the string
+input_data="${input_data#'{xor}'}"
+
+# Decode the Base64 encoded string
+decoded_data=$(echo -n "$input_data" | base64 -d)
 
 # Initialize the variable to store the XOR operation result
 output=""
@@ -15,27 +23,4 @@ for ((i = 0; i < ${#decoded_data}; i++)); do
 done
 
 # Display the result
-echo "$output"st argument passed to the script
-input_data="$1"
-
-# Remove the {xor} prefix from the string
-input_data="${input_data#'{xor}'}"
-
-# Decode the Base64 encoded string
-decoded_data=$(echo -n "$input_data" | openssl enc -base64 -d)
-
-# Initialise la variable pour stocker le résultat de l'opération XOR
-output=""
-
-# Parcourt chaque caractère de la chaîne
-for ((i = 0; i < ${#decoded_data}; i++)); do
-    # Récupère le caractère à la position actuelle
-    char="${decoded_data:$i:1}"
-    # Convertit le caractère en son code ASCII et effectue l'opération XOR avec 95
-    xor_result=$(( $(printf "%d" "'$char") ^ 95 ))
-    # Convertit le résultat XOR en caractère et l'ajoute à la sortie
-    output+=$(printf "\x$(printf '%02x' $xor_result)")
-done
-
-# Affiche le résultat
 echo "$output"
