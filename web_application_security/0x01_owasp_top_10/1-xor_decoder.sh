be983c08d18fe8000428ebb4ba60a46b39363bc8
Pmichel74 <patrick.yann.michel@gmail.com>
2025-09-16 12:35:00 +0200

    task1

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo ${1#*\}} | base64 -d 2>/dev/null | od -An -t u1 | awk '{for(i=1;i<=NF;i++)printf "%c", xor($i,95)}'
+python3 -c "import base64, sys; data = '$1'.split('}', 1)[1]; decoded = base64.b64decode(data); print(''.join(chr(b ^ 95) for b in decoded), end='')" 2>/dev/null || true
