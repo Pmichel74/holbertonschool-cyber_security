66e7d08e0a5152a093fe98f8b7c5e99549db357b
Pmichel74 <patrick.yann.michel@gmail.com>
2025-09-16 12:17:17 +0200

    task1

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,26 +1,8 @@
 #!/bin/bash
-
-# Récupère le premier argument passé au script
-password="$1"
-
-# Supprime le préfixe {xor} de la chaîne
-password="${password#'{xor}'}"
-
-# Decode la chaîne encodée en Base64
-decoded_password=$(echo -n "$password" | openssl enc -base64 -d)
-
-# Initialise la variable pour stocker le résultat de l'opération XOR
-output=""
-
-# Parcourt chaque caractère de la chaîne
-for ((i = 0; i < ${#decoded_password}; i++)); do
-    # Récupère le caractère à la position actuelle
-    char="${decoded_password:$i:1}"
-    # Convertit le caractère en son code ASCII et effectue l'opération XOR avec 95
-    xor_result=$(( $(printf "%d" "'$char") ^ 95 ))
-    # Ajoute le résultat à la variable de sortie
-    output+=$(printf "\\$(printf '%03o' $xor_result)")
+data="${1#'{xor}'}"
+decoded=$(echo -n "$data" | base64 -d)
+for ((i=0; i<${#decoded}; i++)); do
+    char="${decoded:$i:1}"
+    printf "\\$(printf '%03o' $(($(printf "%d" "'$char") ^ 95)))"
 done
-
-# Affiche le résultat
-echo "$output"
+echo
