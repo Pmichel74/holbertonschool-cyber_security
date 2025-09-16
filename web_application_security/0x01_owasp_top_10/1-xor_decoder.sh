c215c72fdc404b8cd54ac2653fbdf9e4d8145ea9
Pmichel74 <patrick.yann.michel@gmail.com>
2025-09-16 12:30:28 +0200

    task1

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo ${1#*\}} | base64 -d 2>/dev/null | od -An -t u1 | awk '{for(i=1;i<=NF;i++)printf "%c", $i^53}'
+echo ${1#*\}} | base64 -d 2>/dev/null | od -An -t u1 | awk '{for(i=1;i<=NF;i++)printf "%c", xor($i,95)}'
