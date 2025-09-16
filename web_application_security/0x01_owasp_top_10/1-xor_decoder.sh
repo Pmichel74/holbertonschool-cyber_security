d7a832e0a59575da9fd4cb7b8fca69a0065a24f3
Pmichel74 <patrick.yann.michel@gmail.com>
2025-09-16 12:09:08 +0200

    task1

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,12 +1,27 @@
-#!/bin/bash
+#!/bin/b# Decode the Base64 encoded string
+decoded_data=$(echo -n "$input_data" | openssl enc -base64 -d)
 
-# Récupère le premier argument passé au script
+# Initialize the variable to store the XOR operation result
+output=""
+
+# Loop through each character of the string
+for ((i = 0; i < ${#decoded_data}; i++)); do
+    # Get the character at the current position
+    char="${decoded_data:$i:1}"
+    # Convert the character to its ASCII code and perform XOR operation with 95
+    xor_result=$(( $(printf "%d" "'$char") ^ 95 ))
+    # Convert the XOR result to character and add it to the output
+    output+=$(printf "\x$(printf '%02x' $xor_result)")
+done
+
+# Display the result
+echo "$output"st argument passed to the script
 input_data="$1"
 
-# Supprime le préfixe {xor} de la chaîne
+# Remove the {xor} prefix from the string
 input_data="${input_data#'{xor}'}"
 
-# Decode la chaîne encodée en Base64
+# Decode the Base64 encoded string
 decoded_data=$(echo -n "$input_data" | openssl enc -base64 -d)
 
 # Initialise la variable pour stocker le résultat de l'opération XOR
