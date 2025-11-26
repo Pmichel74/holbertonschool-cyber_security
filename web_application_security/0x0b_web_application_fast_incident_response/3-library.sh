6f1f81bdb76df85779ac17050cd5797fe8357b69
Pmichel74 <patrick.yann.michel@gmail.com>
2025-11-26 16:26:53 +0100

    task3

--- web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ip_hacker=$(awk '{print $1}' logs.txt | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}')
-grep "$ip_hacker" logs.txt | awk -F'"' '{print $NF}' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
+grep "$ip_hacker" logs.txt | awk -F'"' '{print $6}' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
