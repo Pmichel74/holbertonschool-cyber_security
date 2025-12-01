9d72ff9c4c6d0f3ae03a79bae496cfc939b45c4f
Pmichel74 <patrick.yann.michel@gmail.com>
2025-12-01 15:24:24 +0100

    task2

--- web_application_security/0x0c_web_application_foresics/2-accounts.sh
+++ web_application_security/0x0c_web_application_foresics/2-accounts.sh
@@ -1,1 +1,16 @@
-tail -n 1000 auth.log | grep -E "root" | sort | uniq -c | sort -nr | head -n 1 | awk '{print $12}'
+tail -n 1000 auth.log | awk '
+/Failed password for/ {
+    user = $(NF-5)
+    ip = $(NF-3)
+    key = user "@" ip
+    fails[key]++
+}
+/Accepted password for/ {
+    user = $(NF-5)
+    ip = $(NF-3)
+    key = user "@" ip
+    if (fails[key] >= 3) {
+        print "suspect acitivity : " key " (au moins 3 échecs suivis d\'un succès)"
+    }
+}
+'
