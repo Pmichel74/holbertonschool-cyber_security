d73dc95ccedbecf3394a247f3921db5ab621606b
Pmichel74 <patrick.yann.michel@gmail.com>
2025-12-01 15:27:00 +0100

    task2

--- web_application_security/0x0c_web_application_foresics/2-accounts.sh
+++ web_application_security/0x0c_web_application_foresics/2-accounts.sh
@@ -1,4 +1,4 @@
 tail -n 1000 auth.log | awk '
-/Failed password for/ { fails[$12]++ }
-/Accepted password for/ { if (fails[$12] >= 3 && !reported[$12]) { print $12; reported[$12]=1 } }
+/Failed password for/ { fails[$9]++ }
+/Accepted password for/ { if (fails[$12] >= 3 && !reported[$9]) { print $12; reported[$12]=1 } }
 '
