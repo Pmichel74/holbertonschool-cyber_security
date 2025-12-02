ba5fd67d93b1362e211f4fcf4a074fd156d1beb6
Pmichel74 <patrick.yann.michel@gmail.com>
2025-12-02 09:56:11 +0100

    task5

--- web_application_security/0x0c_web_application_foresics/5-users.sh
+++ web_application_security/0x0c_web_application_foresics/5-users.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -E "new user" auth.log | awk '{print $8}' | sed 's/name=//' | ses 's/,$//' | sort | uniqu | paste -sd ","
+grep -E "new user" auth.log | awk '{print $8}' | sed 's/name=//' | sed 's/,$//' | sort | uniq | paste -sd ","
